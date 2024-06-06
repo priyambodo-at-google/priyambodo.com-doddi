ed4f1df55bb728af91d93da0f694f99af8ab668b
Doddi Priyambodo <doddi@bicarait.com>
2024-06-07 02:59:19 +0700

    Doddi Priyambodo is committing at 2024-06-07 02:59:19

--- doddi.priyambodo.com/src/components/component.tsx
+++ doddi.priyambodo.com/src/components/component.tsx
@@ -191,7 +191,7 @@ export function Component() {
               <Badge variant="secondary">https://www.instagram.com/doddipriyambodo</Badge>
             </div>
           </div>
-          "Kindness is a language which the deaf can hear and the blind can see" ~ Mark Twain
+          Kindness is a language which the deaf can hear and the blind can see ~ Mark Twain
         </div>
       </section>
     </div>
