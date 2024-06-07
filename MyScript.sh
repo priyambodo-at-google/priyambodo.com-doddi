6f88e7629d868ea61bdd60cafdcbe733224a9c89
Doddi Priyambodo <doddi@bicarait.com>
2024-06-07 10:46:34 +0700

    Doddi Priyambodo auto deploy CICD at 2024-06-07 10:46:34

--- MyScript.sh
+++ MyScript.sh
@@ -30,6 +30,7 @@ export AR_REPO='iamdoddi-artifactregistry'
 export SERVICE_NAME='iamdoddi' 
 gcloud artifacts repositories create "$AR_REPO" --location="$GCP_REGION" --repository-format=Docker
 gcloud auth configure-docker "$GCP_REGION-docker.pkg.dev"
+npm run build
 gcloud builds submit --tag "$GCP_REGION-docker.pkg.dev/$GCP_PROJECT/$AR_REPO/$SERVICE_NAME"
 gcloud run deploy "$SERVICE_NAME" \
    --image="$GCP_REGION-docker.pkg.dev/$GCP_PROJECT/$AR_REPO/$SERVICE_NAME" \
@@ -40,9 +41,14 @@ gcloud run deploy "$SERVICE_NAME" \
    --project=$GCP_PROJECT \
    --set-env-vars=GCP_PROJECT=$GCP_PROJECT,GCP_REGION=$GCP_REGION
 
-#Update GitHub
+#Update GitHub to Source Code
 git config --global user.name "Doddi Priyambodo"
 git config --global user.email "doddi@bicarait.com"
 git add .
 git commit -a -m "Doddi Priyambodo is committing at $(date +"%Y-%m-%d %H:%M:%S")"
 git push
+
+#Auto Deploy using Cloud Build
+git add .
+git commit -a -m "Doddi Priyambodo auto deploy CICD at $(date +"%Y-%m-%d %H:%M:%S")"
+git push origin cicd
